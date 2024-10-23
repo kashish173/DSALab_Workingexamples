cf33ad69e2b8fc3c1ce7a94b0862a4f3b38045ca
kashish173 <kashishratnani899@gmail.com>
2024-10-23 08:41:35 -0700

    Add files via upload

---
+++ Task 1.java
@@ -0,0 +1,10 @@
+public class sumOfNaturalNumbers {
+    public static void main(String[] args) {
+        int sum=0;
+        for(int i=1; i<=10; i++) {
+            sum+=i;
+
+        }
+        System.out.println("the sum of first natural numbers is " +sum);
+    }
+}
