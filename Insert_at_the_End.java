adfc31060b1c55238030bc800b8fba8c6a9b4d04
kashish173 <kashishratnani899@gmail.com>
2024-10-31 12:49:37 -0700

    Add files via upload

---
+++ Insert_at_the_End.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DSA;
+
+/**
+ *
+ * @author USER
+ */
+public class Insert_at_the_End {
+    static int insertEnd(int arr[], int n, int key, int capacity){
+        if (n >= capacity)
+            return n; 
+        arr[n] = key;
+        return (n+1);
+    }
+    public static void main(String[] args){
+        int[] arr = new int[20];
+        arr[0] = 12;
+        arr[1] = 16;
+        arr[2] = 20;
+        arr[3] = 40;       
+        arr[4] = 50;       
+        arr[5] = 70;      
+        int capacity = 20;
+        int n = 6;
+        int i, key = 26;
+        
+        System.out.println("Before Insertion: ");
+        for(i = 0; i < n; i++)
+            System.out.print(arr[i] + "");
+        n = insertEnd(arr,n,key,capacity);
+         System.out.println("\nAfter Insertion");
+         for (i=0;i<n;i++)
+         System.out.print(arr[i] + " ");
+    }
+}
+
